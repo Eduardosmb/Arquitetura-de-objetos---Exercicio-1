c981740aca73bdb0da21bf0659f04ab2d65c1b52
Eduardosmb <edubarros253@gmail.com>
2023-02-23 16:05:43 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
 import usuario.Usuario;
+import Mensagem.Mensagem;
+import Mensagem.MensagemTexto;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Usuario magno = new Usuario("Magno", "magno.magno@magno.magno");
+        Usuario celao = new Usuario("Celao", "celao.celao@elon.ma");
+        
+        Mensagem mensagem = new MensagemTexto("23/02/2023", magno, "Amata quinta?", celao);
+
+        System.out.println(mensagem);
+
     }
 }
